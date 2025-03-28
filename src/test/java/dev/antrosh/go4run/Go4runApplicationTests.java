c9a9230e84c62f6c9905db36c180b0e7c9bbdd1d
Arrow2dknee <yozziedummy@gmail.com>
2025-03-28 23:16:20 +0530

    Initial commit of starter files

---
+++ src/test/java/dev/antrosh/go4run/Go4runApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.antrosh.go4run;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Go4runApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
